fb977517ad51c080bbf25c4df5a9c7988e4d612f
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-04 11:36:11 +0700

    09 cookie primer

--- node-express-jwt-auth/app.js
+++ node-express-jwt-auth/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
 const authRoutes = require('./routes/authRoutes');
 
 // express
@@ -8,7 +10,7 @@ const app = express();
 // middleware
 app.use(express.static('public'));
 app.use(express.json());
-
+app.use(cookieParser());
 // view engine
 app.set('view engine', 'ejs');
 
@@ -22,3 +24,21 @@ mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true, useCr
 app.get('/', (req, res) => res.render('home'));
 app.get('/smoothies', (req, res) => res.render('smoothies'));
 app.use(authRoutes);
+
+app.get('/set-cookies', (req, res) => {
+  // res.setHeader('Set-cookie', 'newUser=true');
+
+  res.cookie('newUser', false);
+  res.cookie('isEmployee', true, { maxAge: 1000 * 60 * 60 * 24, httpOnly: true })
+
+  res.send('you got the cookie');
+});
+
+app.get('/read-cookies', (req, res) => {
+  // si cookie bisa diakses karena kita pake cookieParser coyyy...
+  // kirain teh pedah siga akses kana inspect console kitu 
+  const cookies = req.cookies;
+  console.log(cookies);
+
+  res.send(cookies);
+});
