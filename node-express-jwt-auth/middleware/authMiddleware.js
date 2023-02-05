548ae8864ce07b0aa141e1b947cb3fc72baf2ab5
hidayahapriliansyah <adimuhamadfirmansyah@gmail.com>
2023-02-05 14:22:13 +0700

    17 checking the current user

--- node-express-jwt-auth/middleware/authMiddleware.js
+++ node-express-jwt-auth/middleware/authMiddleware.js
@@ -1,4 +1,5 @@
 const jwt = require('jsonwebtoken');
+const User = require('../models/User');
 
 const requireAuth = (req, res, next) => {
   // get cookies jwt
@@ -19,4 +20,27 @@ const requireAuth = (req, res, next) => {
   }
 };
 
-module.exports = { requireAuth };
+const checkUser = (req, res, next) => {
+  const token = req.cookies.jwt;
+
+  if (token) {
+    jwt.verify(token, 'net ninja hash', async (err, decodedToken) => {
+      if (err) {
+        console.log(err.message);
+        res.locals.user = null;
+        next();
+      } else {
+        console.log(decodedToken);
+        let user = await User.findById(decodedToken.id);
+        // locals berfungsi untuk mengirim data mana yang akan dikirim ke view
+        res.locals.user = user;
+        next();
+      }
+    });
+  } else {
+    res.locals.user = null;
+    next();
+  }
+};
+
+module.exports = { requireAuth, checkUser };
